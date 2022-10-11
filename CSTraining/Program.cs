ad0fe7a53723f4b28ba35f8e427b09cc6c71a4d7
Laura Baltutyte <Laura.Baltutyte@oag.com>
2022-10-11 15:55:23 +0300

    Add project files.

---
+++ CSTraining/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Chose operation: + - * /");
+string chosenOperator = Console.ReadLine();
+Console.WriteLine("Enter first number");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter first number");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int answer;
+
+switch(chosenOperator)
+{
+    case "+":
+        answer = number1 + number2;
+        Console.WriteLine("Answer is "+ answer);
+        break;
+    case "-":
+        answer = number1 - number2;
+        Console.WriteLine("Answer is " + answer);
+        break;
+    case "*":
+        answer = number1 * number2;
+        Console.WriteLine("Answer is " + answer);
+        break;
+    case "/":
+        answer = number1 / number2;
+        Console.WriteLine("Answer is " + answer);
+        break;
+    default:
+        Console.WriteLine("WTF. Try again");
+        break;
+}
