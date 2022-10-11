3c7c520ddbfa0552d1f18cdd426f96254fe46de7
Laura Baltutyte <Laura.Baltutyte@oag.com>
2022-10-11 18:08:24 +0300

    Updated code

--- CSTraining/Program.cs
+++ CSTraining/Program.cs
@@ -1,31 +1,41 @@
-﻿Console.WriteLine("Choose operation: + - * /");
-string chosenOperator = Console.ReadLine();
-Console.WriteLine("Enter first number");
-int number1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter first number");
-int number2 = Convert.ToInt32(Console.ReadLine());
-
-int answer;
-
-switch(chosenOperator)
+﻿namespace CSTraining
 {
-    case "+":
-        answer = number1 + number2;
-        Console.WriteLine("Answer is "+ answer);
-        break;
-    case "-":
-        answer = number1 - number2;
-        Console.WriteLine("Answer is " + answer);
-        break;
-    case "*":
-        answer = number1 * number2;
-        Console.WriteLine("Answer is " + answer);
-        break;
-    case "/":
-        answer = number1 / number2;
-        Console.WriteLine("Answer is " + answer);
-        break;
-    default:
-        Console.WriteLine("WTF. Try again");
-        break;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose operation: + - * /");
+            string chosenOperator = Console.ReadLine();
+            if (chosenOperator == "+"
+                   || chosenOperator == "-"
+                   || chosenOperator == "*"
+                   || chosenOperator == "/")
+            {
+                Console.WriteLine("Enter first number");
+                int number1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter second number");
+                int number2 = Convert.ToInt32(Console.ReadLine());
+
+                string result = Calculator(number1, number2, chosenOperator);
+                Console.WriteLine("Answer is " + result);
+            }
+            else
+            {
+                Console.WriteLine("WTF. Try again");
+            }
+
+        }
+        public static string Calculator (int number1, int number2, string chosenOperator)
+        {
+            return chosenOperator switch
+            {
+                "+" => Convert.ToString(number1 + number2),
+                "-" => Convert.ToString(number1 - number2),
+                "*" => Convert.ToString(number1 * number2),
+                "/" => Convert.ToString(number1 / number2),
+                _ => "WTF. Try again",
+            };
+        }
+    }
 }
