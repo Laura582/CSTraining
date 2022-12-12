f55d85fbfd02fda13bbe1e414040e53461c1d647
Laura Baltutyte <baltutytelaura@gmail.com>
2022-12-12 15:58:56 +0200

    Collection homework

---
+++ Collections/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Enter sentence");
+
+string sentence = Console.ReadLine();
+string sentenceToUpper = sentence.ToUpper();
+
+string[] words = sentenceToUpper.Split(' ');
+
+Dictionary<char, int> wordCounts = new Dictionary<char, int>();
+
+foreach (string word in words)
+{
+    char[] charArr = word.ToCharArray();
+
+    if (!wordCounts.ContainsKey(charArr[0]))
+    {
+        wordCounts[charArr[0]] = 0;
+    }
+    wordCounts[charArr[0]]++;
+}
+
+foreach (char letter in wordCounts.Keys)
+{
+    Console.WriteLine($"{letter}: {wordCounts[letter]}");
+}
