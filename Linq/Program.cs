2b2eb05bb49a934d84b3f3c1a11cd9831aa0e3c1
Laura Baltutyte <baltutytelaura@gmail.com>
2022-12-19 10:34:09 +0200

    Linq homework

---
+++ Linq/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Enter sentence");
+
+string sentence = Console.ReadLine();
+
+string[] words;
+
+if (sentence != null)
+{ 
+    words = sentence.Split(' ');
+    var stringList = new List<string>(words);
+    var numbers = stringList
+    .Where(x => int.TryParse(x, out int n) == true)
+    .Select(x => int.Parse(x))
+    .Select(x => Math.Pow(x, 2))
+    .OrderByDescending(x => x);
+    PrintCollection(numbers);
+}
+else
+{
+    Console.WriteLine("You did not enter a sentence. Try again");
+}
+
+void PrintCollection(IEnumerable<double> doubles)
+{
+    foreach (var i in doubles)
+    { 
+        Console.WriteLine(i);
+    }
+}
