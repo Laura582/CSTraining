90f95bb69870a5133f3c78e30d5af541a48114d5
Laura Baltutyte <baltutytelaura@gmail.com>
2023-01-09 10:00:05 +0200

    Added dependence

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -1,29 +1,34 @@
-﻿Console.WriteLine("Enter sentence");
-
-string sentence = Console.ReadLine();
-
-string[] words;
-
-if (sentence != null)
-{ 
-    words = sentence.Split(' ');
-
-    var numbers = words
-        .Where(x => int.TryParse(x, out int n))
-        .Select(x => Math.Pow(int.Parse(x), 2))
-        .OrderByDescending(x => x);
-
-    PrintCollection(numbers);
-}
-else
+﻿namespace Linq
 {
-    Console.WriteLine("You did not enter a sentence. Try again");
-}
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter sentence");
 
-void PrintCollection(IEnumerable<double> doubles)
-{
-    foreach (var i in doubles)
-    { 
-        Console.WriteLine(i);
+            string sentence = Console.ReadLine();
+
+            string[] words;
+
+            if (sentence != null)
+            {
+                words = sentence.Split(' ');
+
+
+                 PrintCollection(LinqLibrary.LinqLibraries.CalculateNumbers(words));
+            }
+            else
+            {
+                 Console.WriteLine("You did not enter a sentence. Try again");
+            }
+        }
+
+        static void PrintCollection(IEnumerable<double> doubles)
+        {
+            foreach (var i in doubles)
+            {
+                Console.WriteLine(i);
+            }
+        }
     }
 }
---
+++ LinqLibrary/LinqLibraries.cs
@@ -0,0 +1,15 @@
+﻿namespace LinqLibrary
+{
+    public class LinqLibraries
+    {
+        public static IOrderedEnumerable<double> CalculateNumbers(string[] sentence)
+        {
+            var calculatedNumbers = sentence
+                .Where(x => int.TryParse(x, out int n))
+                .Select(x => Math.Pow(int.Parse(x), 2))
+                .OrderByDescending(x => x);
+
+            return calculatedNumbers;
+        }
+    }
+}
